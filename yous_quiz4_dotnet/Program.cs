5d25dd228ed4441118918132b48ce4df4f991981
Yousif Albayati <yalbayat@my.centennialcollege.ca>
2025-04-07 12:25:54 -0400

    Initial commit

---
+++ yous_quiz4_dotnet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yous_quiz4_dotnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
